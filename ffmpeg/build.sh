91280268ce0a191f48bbf5ecc751f9cb8ebb82f4
Sam <ffxsam@users.noreply.github.com>
2019-11-04 16:21:49 -0600

    Initial commit

---
+++ ffmpeg/build.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -ex
+
+# Build libmp3lame
+wget https://sourceforge.net/projects/lame/files/lame/3.100/lame-3.100.tar.gz/download -O lame.tar.gz
+tar xf lame.tar.gz
+cd lame-3.100
+./configure --disable-shared --enable-static --libdir=/lib64
+make install
+cd /
+
+git clone https://github.com/FFmpeg/FFmpeg.git
+cd FFmpeg
+./configure --disable-everything --disable-doc --enable-static --enable-small \
+  --enable-decoder=pcm*,flac,mp3,wavpack --enable-libmp3lame \
+  --enable-encoder=libmp3lame --enable-muxer=hls,segment \
+  --enable-demuxer=wav,mp3 --enable-protocol=file,pipe \
+  --enable-filter=aresample,afade
+make
