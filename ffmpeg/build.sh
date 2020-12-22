889602af55360f811bac8661d1fc7db6312ccadb
Sam Hulick <samh@reelcrafter.com>
2020-12-21 21:10:49 -0600

    Expanded support of formats
    Larger filesize now. Need to figure out how to limit to audio only

--- ffmpeg/build.sh
+++ ffmpeg/build.sh
@@ -12,9 +12,11 @@ cd /
 
 git clone https://github.com/FFmpeg/FFmpeg.git
 cd FFmpeg
-./configure --disable-everything --disable-doc --enable-static --enable-small \
-  --enable-decoder=pcm*,flac,mp3,wavpack --enable-libmp3lame \
-  --enable-encoder=libmp3lame --enable-muxer=hls,segment,tee,mp3 \
-  --enable-demuxer=wav,mp3 --enable-protocol=file,pipe \
-  --enable-filter=aresample,afade
+#./configure --disable-everything --disable-doc --enable-static --enable-small \
+#  --enable-decoder=pcm*,flac,mp3,wavpack,aac --enable-libmp3lame \
+#  --enable-encoder=libmp3lame --enable-muxer=hls,segment,tee,mp3 \
+#  --enable-demuxer=wav,mp3 --enable-protocol=file,pipe \
+#  --enable-filter=aresample,afade
+./configure --disable-doc --disable-shared --enable-static --enable-libmp3lame \
+  --enable-gpl --enable-nonfree
 make
