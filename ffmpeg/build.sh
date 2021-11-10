478a6b40e270da873cbef58b20007af19deabbc0
Sam Hulick <samh@reelcrafter.com>
2021-11-10 02:09:33 -0600

    Added WAV muxer to build config

--- ffmpeg/build.sh
+++ ffmpeg/build.sh
@@ -16,7 +16,7 @@ git checkout n4.4 # Only build stable releases
 ./configure --disable-everything --disable-doc --enable-static \
   --enable-decoder=pcm*,flac,mp3,wavpack,aac,mjpeg --enable-libmp3lame --enable-gpl \
   --enable-nonfree --enable-encoder=libmp3lame \
-  --enable-muxer=hls,segment,tee,mp3,ffmetadata,mjpeg,image2 \
+  --enable-muxer=hls,segment,tee,mp3,ffmetadata,mjpeg,image2,wav \
   --enable-demuxer=wav,mp3,mov,aiff,ffmetadata,mjpeg,image2 --enable-protocol=file,pipe \
   --enable-filter=aresample,afade
 make
